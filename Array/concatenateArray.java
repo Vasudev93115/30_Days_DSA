e9901b3c591ceea5116ec4ea379b4a74aa9f5470
Vasudev93115 <girkaran925@gmail.com>
2026-01-25 17:00:59 +0530

    Day-2

---
+++ Array/concatenateArray.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class concatenateArray {
+    public static void main(String[] args) {
+        int[] arr={2,4,5};
+        int n=arr.length;
+        int[] ans=new int[2*n];
+        for(int i=0;i<n;i++){
+            ans[i]=arr[i];
+            ans[i+n]=arr[i];
+        }
+        for(int i=0;i<2*n;i++){
+            System.out.print(ans[i]+", ");
+        }
+    }
+}
